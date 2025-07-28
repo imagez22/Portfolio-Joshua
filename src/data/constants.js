364e33576bb884eda1d0631a858184577ceb9cee
Akli Joshua Eric Yao <63812022+imagez22@users.noreply.github.com>
2025-07-28 16:09:52 +0000

    left with projects and last section

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,19 +1,18 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Akli Joshua Eric Yao",
   roles: [
-    "Full Stack Developer",
-    "Android Developer",
-    "UI/UX Designer",
-    "Programmer",
+    "Front End Web Developer",
+    "Lincensed Trained Teacher",
+    "IT Technical supporter",
+    "Network administrator",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/imagez22",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+    "https://drive.google.com/file/d/1IILHgvG07BXoK1wU8KaU4Gl7WeUdriWO/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/akli-joshua-eric-yao-306b85364/",
   twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
   facebook: "https://www.facebook.com/rishav.chanda.165/",
 };
 
